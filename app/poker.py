678658ee70e4b1867a6a31f0d64a060ccba60ed1
Jake Liu <jakeliu444@gmail.com>
2025-12-10 11:47:41 -0500

    worked on deal_hole

--- app/poker.py
+++ app/poker.py
@@ -7,13 +7,20 @@ class Poker:
         )
         data = response.json()
         self.deck_id = data["deck_id"]
+        self.board = []
+        self.hole_cards = []
 
-    def deal(self):
-        return requests.get(
-            f"https://deckofcardsapi.com/api/deck/{self.deck_id}/draw/?count=1"
+    def deal_hole(self):
+        card_info = requests.get(
+            f"https://deckofcardsapi.com/api/deck/{self.deck_id}/draw/?count=4"
         ).json()["cards"]
+        dealt_cards = [card_info[i]["code"] for i in range(len(card_info))]
+        print(dealt_cards)
+        self.hole_cards.append(dealt_cards[0::2])
+        self.hole_cards.append(dealt_cards[1::2])
+        print(hole_cards)
 
 if __name__ == "__main__":
     game = Poker()
     print(game.deck_id)
-    print(game.deal())
+    print(game.deal_hole())
